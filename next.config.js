0797c463e44c06ddbef45dc31d157c5613905ead
Akshay Bhat <bhat7362@gmail.com>
2022-07-03 17:05:55 +0530

    bruu

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withMDX = require("@next/mdx")({
   extension: /\.md$/,
 });
 module.exports = withMDX({
+  devIndicator: { buildAcitivity: false },
   pageExtensions: ["js", "jsx", "md", "mdx"],
   webpack: (config, { isServer }) => {
     // Fixes npm packages (mdx) that depend on `fs` module
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import Theme from "../components/themes";
 import "../styles/global.css";
 import MDX from "../components/mdx";
-import { MDXProvider } from "@mdx-js/react";
 
 const colors = {};
 const theme = extendTheme({ colors });
@@ -10,9 +9,7 @@ const theme = extendTheme({ colors });
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={Theme}>
-      <MDXProvider components={MDX}>
         <Component {...pageProps} />
-      </MDXProvider>
     </ChakraProvider>
   );
 }
